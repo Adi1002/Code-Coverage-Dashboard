0298e4a4c4b3d16a587397dca614d73aad8a5dac
Aditya Kumar Singh <adityasingh.dpsn@gmail.com>
2026-06-22 14:35:31 +0530

    vercel proxy - 2

--- Adi15/api/repository.js
+++ Adi15/api/repository.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   try {
     const response = await fetch(
-      'http://ec2-100-24-9-250.compute-1.amazonaws.com/data/repository'
+      "http://ec2-100-24-9-250.compute-1.amazonaws.com/data/repository"
     );
 
     const data = await response.json();
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({
-      error: error.message
+      error: error.message,
     });
   }
 }
