65066e9418cdfc201f6c30164821a07f298670fc
Aditya Kumar Singh <adityasingh.dpsn@gmail.com>
2026-06-23 17:00:55 +0530

    proxy_change-1

--- Adi15/api/summary.js
+++ Adi15/api/summary.js
@@ -1,17 +1,19 @@
+// api/summary.js
+
 export default async function handler(req, res) {
   try {
+    const { service } = req.query;
+
     const response = await fetch(
-      "https://ec2-100-24-9-250.compute-1.amazonaws.com:8000/data/repository"
+      `http://13.127.42.153/codecoverage/dashboard/${service}/summary`
     );
 
     const data = await response.json();
 
     return res.status(200).json(data);
   } catch (error) {
-    console.error(error);
-
     return res.status(500).json({
-      message: error.message,
+      error: error.message,
     });
   }
 }
