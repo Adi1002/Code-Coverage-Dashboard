72154011a5d2b267805945c966e24e2df5482143
Aditya Kumar Singh <adityasingh.dpsn@gmail.com>
2026-07-15 13:11:55 +0530

    final_change_final

--- Adi15/vite.config.js
+++ Adi15/vite.config.js
@@ -3,21 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // 1. Target One: Routes starting with '/data' go to Port 8000
-      '/data': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-      },
-      
-      // 2. Target Two: Routes starting with '/codecoverage' go to Port 8001
-      '/codecoverage': {
-        target: 'http://127.0.0.1:8001',
-        changeOrigin: true,
-      }
-    }
-  },
   
   // NEW: Code Splitting Configuration
   build: {
